d55ed7cae9152867aec58376a52ab16dafeb017e
Warlisson Miranda <warlisson@ymail.com>
2022-11-28 21:34:14 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buttons - Tailwind CSS</title>
+
+    <link href="./dist/output.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="flex flex-col items-center justify-center h-screen space-y-6">
+
+        <div class="flex items-center space-x-2">
+            <button class="text-sm px-3 py-1.5 bg-indigo-500 text-white rounded-lg outline-none focus:ring-4 focus:ring-indigo-200 hover:bg-indigo-600">
+                Button
+            </button>
+            <button class="px-5 py-2 bg-indigo-500 text-white rounded-lg outline-none focus:ring-4 focus:ring-indigo-200 hover:bg-indigo-600">
+                Button
+            </button>
+            <button class="text-lg px-6 py-3 bg-indigo-500 text-white rounded-lg outline-none focus:ring-4 focus:ring-indigo-200 hover:bg-indigo-600">
+                Button
+            </button>
+        </div>
+
+        <div class="flex items-center space-x-2">
+            <button class="text-sm px-3 py-1.5 bg-rose-500 text-white rounded-lg outline-none focus:ring-4 focus:ring-rose-200 hover:bg-rose-600">
+                Button
+            </button>
+            <button class="px-5 py-2 bg-rose-500 text-white rounded-lg outline-none focus:ring-4 focus:ring-rose-200 hover:bg-rose-600">
+                Button
+            </button>
+            <button class="text-lg px-6 py-3 bg-rose-500 text-white rounded-lg outline-none focus:ring-4 focus:ring-rose-200 hover:bg-rose-600">
+                Button
+            </button>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
